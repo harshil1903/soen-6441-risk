93841052556cea99b55d057edbe706b26fa5c3f4
Harshil Patel <harshilpatel1903@gmail.com>
2021-02-08 10:21:33 +0530

    Initial Commit

---
+++ src/main/java/com/risk/main/Main.java
@@ -0,0 +1,17 @@
+package com.risk.main;
+
+
+
+/**
+ * This is main entrance point of the program. The execution of the game begins from here.
+ * @author Harshil
+ *
+*/
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Test");
+    }
+
+}
