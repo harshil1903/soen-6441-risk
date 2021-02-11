44ce6e2cd97220d6b006118c14c3b0b885cb0319
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-02-11 22:30:07 +0530

    Imported required file and updated data member.

--- src/main/java/com/risk/maputils/EditMap.java
+++ src/main/java/com/risk/maputils/EditMap.java
@@ -1,6 +1,15 @@
 package com.risk.maputils;
-
 import com.risk.models.Map;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import com.risk.models.Continent;
+import com.risk.models.Country;
+import java.io.FileInputStream;
+
 
 /**
 * This class loads a map from an existing domination map file,
@@ -10,42 +19,29 @@ import com.risk.models.Map;
 
 public class EditMap {
     // Object map that will be returned after processing.
-    private Map c_Map;
+    private Map d_Map;
 
     /**
      * Default constructor
      */
-    public EditMap(){}
+    public EditMap() {
+    }
 
     /**
      * Parameterized constructor
+     *
      * @param p_map provides the map for loading
      */
     public EditMap(Map p_map) {
-        this.c_Map = p_map;
+        this.d_Map = p_map;
     }
 
     /**
      * return map object after loading or creating if doesn't exist.
+     *
      * @return the map
      */
-    private Map GetMap(){
-        return c_Map;
+    private Map GetMap() {
+        return d_Map;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
