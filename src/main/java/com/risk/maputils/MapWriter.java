779ad0e5d6c401f51c3fc2f408d19eb421105853
Rishabh <rishabhpatel12121@gmail.com>
2021-02-12 17:08:33 +0530

    Created processContinent method to process the continents

--- src/main/java/com/risk/maputils/MapWriter.java
+++ src/main/java/com/risk/maputils/MapWriter.java
@@ -1,5 +1,6 @@
 package com.risk.maputils;
 
+import com.risk.models.Continent;
 import com.risk.models.Map;
 
 import java.io.File;
@@ -45,27 +46,44 @@ public class MapWriter {
 
         StringBuilder content = new StringBuilder();
         content = processMapAttribute(map);
-/*        content.append(processContinent(map));
-        content.append(processCountries(map)); */
+        content.append(processContinent(map));
+       /* content.append(processCountries(map));*/
         return content.toString();
     }
+        /**
+     * This method processes map attributes.
+     * @param map object of the map is being processed
+     * @return a String that contains map properties.
+     */
 
-/**
- * This method processes map attributes.
- * @param map object of the map is being processed
- * @return a String that contains map properties.
- */
-
-private StringBuilder processMapAttribute(Map map){
-    StringBuilder mapAttribute = new StringBuilder();
-    mapAttribute.append("[Map]");
-    mapAttribute.append("\n");
-/*
-    for(Entry<String, String> keymap: map.getD_MapData().entrySet()){
-        mapAttribute.append(keymap.getKey() + "=" + keymap.getValue());
+    private StringBuilder processMapAttribute(Map map){
+        StringBuilder mapAttribute = new StringBuilder();
+        mapAttribute.append("[Map]");
         mapAttribute.append("\n");
-*/
-    return mapAttribute;
-}
-}
 
+        for(java.util.Map.Entry<String, String> keymap: map.getD_MapData().entrySet()) {
+            mapAttribute.append(keymap.getKey() + "=" + keymap.getValue());
+            mapAttribute.append("\n");
+        }
+        return mapAttribute;
+    }
+
+    /**
+     * This method processes the continents.
+     * @param map object of the map which is being processed
+     * @return a string that contains details of the continents that will eventually be written in the map file.
+     */
+
+    private StringBuilder processContinent(Map map){
+        StringBuilder continentData = new StringBuilder();
+        continentData.append("\n");
+        continentData.append("[Continents]");
+        continentData.append("\n");
+        for (Continent continent : map.getD_Continents()){
+            continentData.append(continent.getD_ContinentName() + "=" + continent.getD_ContinentValue());
+            continentData.append("\n");
+        }
+        return continentData;
+    }
+
+}
