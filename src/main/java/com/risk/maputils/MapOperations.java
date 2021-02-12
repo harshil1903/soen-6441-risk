6722b75819f991826798d1a3247410ad2b5306d0
PARTHNAVSARIWALA <parthnavsariwala29@gmail.com>
2021-02-12 11:40:43 +0530

    Change methods in map operations according to new map method

--- src/main/java/com/risk/maputils/MapOperations.java
+++ src/main/java/com/risk/maputils/MapOperations.java
@@ -2,6 +2,7 @@ package com.risk.maputils;
 
 import com.risk.exception.InvalidMapException;
 import com.risk.models.Continent;
+import com.risk.models.Country;
 import com.risk.models.Map;
 
 import java.util.ArrayList;
@@ -21,74 +22,65 @@ public class MapOperations {
     /**
      * Add continent to map with details such as ID and Value
      *
-     * @param p_map            current map object
+     * @param p_Map            current map object
      * @param p_ContinentID    ID of the continent
      * @param p_ContinentValue Value of the continent
      * @return Returns the new continent
      * @throws InvalidMapException throws IO Exception if there is any error while doing operations on map
      */
 
-    public static Continent addContinent(Map p_map, int p_ContinentID, int p_ContinentValue) throws InvalidMapException {
-        Continent d_continent = new Continent();
-        d_continent.setD_ContinentID(p_ContinentID);
-        d_continent.setD_ContinentValue(p_ContinentValue);
+    public static Continent addContinent(Map p_Map, int p_ContinentID, int p_ContinentValue) throws InvalidMapException {
+        Continent l_continent = new Continent();
+        l_continent.setD_ContinentID(p_ContinentID);
+        l_continent.setD_ContinentValue(p_ContinentValue);
 
-        if (p_map.getD_Continents().contains(d_continent)) {
+        if (p_Map.getD_Continents().contains(l_continent)) {
             throw new InvalidMapException("The Continent" + p_ContinentID + "Already Exist");
         } else {
-            p_map.getD_Continents().add(d_continent);
+            p_Map.addContinentToContinentList(l_continent);
         }
-        return d_continent;
+        return l_continent;
     }
 
     /**
-     * Remove continent to map with details such as ID and Value
-     *
-     * @param p_map         current map object
+     * Remove continent to map with details such as ID and Valu
+     * @param p_Map current map object
      * @param p_ContinentID ID of the continent
      * @throws InvalidMapException throws IO Exception if there is any error while doing operations on map
      */
 
-    public static void removeContinent(Map p_map, int p_ContinentID) throws InvalidMapException {
-        Continent d_rcontinent = new Continent();
-        if (d_rcontinent.getD_ContinentID() == p_ContinentID) {
-            p_map.getD_Continents().remove(d_rcontinent);
+    public static void removeContinent(Map p_Map, int p_ContinentID) throws InvalidMapException {
+        Continent l_rcontinent = new Continent();
+        l_rcontinent.setD_ContinentID(p_ContinentID);
+        if(p_Map.isContinentPresentInContinentList(l_rcontinent)){
+            p_Map.removeContinentFromContinentList(l_rcontinent);
         }
-
+        else{
+            throw new InvalidMapException("The Continent Not Present In The List");
+        }
     }
 
     /**
      * Add country to map with details such as ID and Value
-     *
-     * @param p_map         current map object
+     * @param p_Map         current map object
      * @param p_CountryID   ID of the country
      * @param p_ContinentID Value of the country
      * @return Returns the new country
      * @throws InvalidMapException throws IO Exception if there is any error while doing operations on map
      */
 
-    /*public static Country addCountry(Map p_map, int p_CountryID, int p_ContinentID) throws InvalidMapException {
-        Country d_country = new Country();
-        List<Country> l_TempCountry=new ArrayList<Country>();
-        d_country.setD_CountryID(p_CountryID);
-        List<Continent> l_Continentlist = new ArrayList<Continent>();
-        l_Continentlist = p_map.getD_Continents();
+    public static Country addCountry(Map p_Map, int p_CountryID, int p_ContinentID) throws InvalidMapException {
+        Country l_country = new Country();
+        Continent l_continent=new Continent();
+        l_country.setD_CountryID(p_CountryID);
+        l_country.setD_ContinentID(p_ContinentID);
+        List<Country> l_tempCountry=new ArrayList<Country>();
 
-        //check whether country exist or not
-        for (Continent l_continent : l_Continentlist) {
-            List<Country> l_countrylist=l_continent.getD_Countries();
-            for(l_TempCountry:l_countrylist) {
-                if (l_TempCountry.get == p_CountryID) {
-                    throw new InvalidMapException("The Continent" + p_ContinentID + "Already Exist");
-                }
-            }
-        }
-        return d_country;
-    }*/
+        return l_country;
+    }
 
     /**
      * Remove continent to map with details such as ID and Value
-     *
      * @param p_map       current map object
      * @param p_CountryID ID of the continent
      * @throws InvalidMapException throws IO Exception if there is any error while doing operations on map
