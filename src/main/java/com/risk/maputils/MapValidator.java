0cb8728c2bef7621282a8b6de264425fc5639a43
HARSH <harshp1898@gmail.com>
2021-02-15 05:09:33 -0600

    Updated the local variables, data members, method parameters according to coding convention

--- src/main/java/com/risk/maputils/MapValidator.java
+++ src/main/java/com/risk/maputils/MapValidator.java
@@ -18,32 +18,32 @@ import com.risk.models.Country;
 
 public class MapValidator {
 
-    static String AlertMessage ="";
+    static String d_AlertMessage ="";
 
     /**
      * This methodis used for validaion of map.
-     * @param map refers to the map object for verification.
+     * @param p_Map refers to the map object for verification.
      * @throws InvalidMapException is used for the situation where map has certain errors.
      */
 
-    public static void validateMap(Map map) throws InvalidMapException {
-        if(map==null){
+    public static void validateMap(Map p_Map) throws InvalidMapException {
+        if(p_Map==null){
             throw new InvalidMapException("The Map is not valid, it does not contain any content.");
         } else{
-            if(map.getD_Continents().size()<1){
+            if(p_Map.getD_Continents().size()<1){
                 throw new InvalidMapException("There should be atleast one Continent in the graph.");
             } else{
                 //A map must be subgraph of continents and if it is true then the continents must be subgraph of countries.
                 //This method will validate whether the map is subgraph of continents or not.
-                validateContinents(map);
+                validateContinents(p_Map);
 
                 //This method will check whether if the map is subgraph of continents.
-                if(!isMapConnectedGraph(map)){
+                if(!isMapConnectedGraph(p_Map)){
                     throw new InvalidMapException("The map is not a connected graph i.e Continent is not a subgraph in the map. A map should be connected graph formed by continents.");
                 }
 
                 //validate whether the country belongs to only one continent.
-                validateCountryBelongsToOneContinent(map);
+                validateCountryBelongsToOneContinent(p_Map);
             }
 
         }
@@ -52,24 +52,24 @@ public class MapValidator {
 
     /**
      * This method is used to validate the continents.
-     * @param map map refers to map object to validate the continents.
+     * @param p_Map map refers to map object to validate the continents.
      * @throws InvalidMapException throws exception if map has any continent related errors.
      */
 
-    public static void validateContinents(Map map) throws InvalidMapException{
+    public static void validateContinents(Map p_Map) throws InvalidMapException{
 
-        for(Continent continent : map.getD_Continents()){
-            if(continent.getD_Countries().size() < 1){
+        for(Continent l_continent : p_Map.getD_Continents()){
+            if(l_continent.getD_Countries().size() < 1){
                 throw new InvalidMapException("There should be atleast one country in any continent.");
             }
             //At this point it is validated that map is subgraph of continents, now validate whether continent is subgraph of countries.
-            for(Country country : continent.getD_Countries()){
-                validateCountry(country,map);
+            for(Country l_country : l_continent.getD_Countries()){
+                validateCountry(l_country,p_Map);
             }
 
             //This method will check whether if the continent is a subgraph of countries.
-            if(!isContinentConnectedGraph(continent,map)){
-                throw new InvalidMapException("The Continent:- "+continent.getD_ContinentName()+" is not connected by its neighbouring countries.A Continent must be connected graph formed by its neighbouring countries in the map.");
+            if(!isContinentConnectedGraph(l_continent,p_Map)){
+                throw new InvalidMapException("The Continent:- "+l_continent.getD_ContinentName()+" is not connected by its neighbouring countries.A Continent must be connected graph formed by its neighbouring countries in the map.");
             }
         }
     }
@@ -77,27 +77,27 @@ public class MapValidator {
 
     /**
      * This method is used for validating that whether the continents are connected or not.
-     * @param continent is the continent to be validated.
+     * @param p_continent is the continent to be validated.
      * @map is the current map object.
      * @return will return true if the continent is connected in the map.
      */
 
-    public static boolean isContinentConnectedGraph(Continent continent,Map map){
-        bfsTraversalCountry(continent.getD_Countries().get(0),map);
-        boolean d_returnValue = true;
-        for(Country c : continent.getD_Countries()){
+    public static boolean isContinentConnectedGraph(Continent p_Continent,Map p_Map){
+        bfsTraversalCountry(p_Continent.getD_Countries().get(0),p_Map);
+        boolean l_returnValue = true;
+        for(Country c : p_Continent.getD_Countries()){
             if(c.isD_IsProcessed() == false){
                 c.setD_Processed(false);
-                AlertMessage = c.getD_CountryName()+"country is not forming connected graph in the continent"+continent.getD_ContinentName()+".";
-                d_returnValue = false;
+                d_AlertMessage = c.getD_CountryName()+"country is not forming connected graph in the continent"+p_Continent.getD_ContinentName()+".";
+                l_returnValue = false;
                 break;
             }
         }
 
-        for(Country c : continent.getD_Countries()){
+        for(Country c : p_Continent.getD_Countries()){
             c.setD_Processed(false);
         }
-        return d_returnValue;
+        return l_returnValue;
     }
 
     /**
@@ -106,16 +106,16 @@ public class MapValidator {
      * @map refers to the map object.
      */
 
-    public static void bfsTraversalCountry(Country country,Map map){
-        if(country.isD_IsProcessed() == true){
+    public static void bfsTraversalCountry(Country p_Country,Map p_Map){
+        if(p_Country.isD_IsProcessed() == true){
             return;
         }
 
-        country.setD_Processed(true);
+        p_Country.setD_Processed(true);
 
-        for(Country c : country.getD_AdjacentCountries()){
-            if((c.getD_BelongToContinent() == country.getD_BelongToContinent()) && c.isD_IsProcessed() == false)
-                bfsTraversalCountry(c,map);
+        for(Country c : p_Country.getD_AdjacentCountries()){
+            if((c.getD_BelongToContinent() == p_Country.getD_BelongToContinent()) && c.isD_IsProcessed() == false)
+                bfsTraversalCountry(c,p_Map);
         }
 
     }
@@ -127,17 +127,17 @@ public class MapValidator {
      * @throws InvalidMapException throws exception if the map is not valid.
      */
 
-    public static void validateCountry(Country country,Map map) throws InvalidMapException{
-        List<Country> adjCountryList = country.getD_AdjacentCountries();
+    public static void validateCountry(Country p_Country,Map p_Map) throws InvalidMapException{
+        List<Country> l_adjCountryList = p_Country.getD_AdjacentCountries();
 
-        if((adjCountryList == null) && (adjCountryList.size()<1)){
-            throw new InvalidMapException("Country: "+country.getD_CountryName()+" must have atleast one adjacent country.");
+        if((l_adjCountryList == null) && (l_adjCountryList.size()<1)){
+            throw new InvalidMapException("Country: "+p_Country.getD_CountryName()+" must have atleast one adjacent country.");
         }
         else{
-            for(Country adjCountry : adjCountryList){
-                if(!adjCountry.getD_AdjacentCountries().contains(country)){
+            for(Country l_adjCountry : l_adjCountryList){
+                if(!l_adjCountry.getD_AdjacentCountries().contains(p_Country)){
                     //add the passed country to the list of adjacent countries if it is not connected to its adjacent country
-                    adjCountry.getD_AdjacentCountries().add(country);
+                    l_adjCountry.getD_AdjacentCountries().add(p_Country);
                 }
             }
         }
@@ -150,27 +150,27 @@ public class MapValidator {
      * @return returns true if it the map is connected graph.
      */
 
-    public static boolean isMapConnectedGraph(Map map){
+    public static boolean isMapConnectedGraph(Map p_Map){
         System.out.println("The map is connected");
-        if(map.getD_Continents().size()<2){
+        if(p_Map.getD_Continents().size()<2){
             return false;
         }
 
-        bfsTraversalContinent(map.getD_Continents().get(0),map);
+        bfsTraversalContinent(p_Map.getD_Continents().get(0),p_Map);
 
-        boolean d_returnvalue = true;
-        for(Continent continent : map.getD_Continents()){
-            if(continent.isD_IsVisited() == false){
-                System.out.println(continent.getD_ContinentName()+"XXXXXX");
-                d_returnvalue = false;
+        boolean l_returnvalue = true;
+        for(Continent l_continent : p_Map.getD_Continents()){
+            if(l_continent.isD_IsVisited() == false){
+                System.out.println(l_continent.getD_ContinentName()+"XXXXXX");
+                l_returnvalue = false;
                 break;
             }
         }
 
-        for(Continent continent : map.getD_Continents()){
-            continent.setD_Visited(false);
+        for(Continent l_continent : p_Map.getD_Continents()){
+            l_continent.setD_Visited(false);
         }
-        return d_returnvalue;
+        return l_returnvalue;
     }
 
     /**
@@ -179,18 +179,18 @@ public class MapValidator {
      * @map refers to the map object
      */
 
-    public static void bfsTraversalContinent(Continent continent,Map map){
-        if(continent.isD_IsVisited()==true){
+    public static void bfsTraversalContinent(Continent p_Continent,Map p_Map){
+        if(p_Continent.isD_IsVisited()==true){
             return;
         }
 
-        continent.setD_Visited(true);
+        p_Continent.setD_Visited(true);
 
-        System.out.println("continent in bfs"+continent.getD_ContinentName());
-        for(Continent c : getAdjacentContinents(continent,map)){
+        System.out.println("continent in bfs"+p_Continent.getD_ContinentName());
+        for(Continent c : getAdjacentContinents(p_Continent,p_Map)){
             System.out.println("Control is inside the adjacent continents loop");
             if(c.isD_IsVisited() == false)
-                bfsTraversalContinent(c,map);
+                bfsTraversalContinent(c,p_Map);
         }
 
     }
@@ -202,29 +202,29 @@ public class MapValidator {
      * @return will return the list of adjacent continents of the current continent.
      */
 
-    public static List<Continent> getAdjacentContinents(Continent continent, Map map){
-        List<Continent> adjacentContinents = new ArrayList<>();
+    public static List<Continent> getAdjacentContinents(Continent p_Continent, Map p_Map){
+        List<Continent> l_adjacentContinents = new ArrayList<>();
 
-        HashSet<Country> adjCountryMainSet = new HashSet<>(); ////hash set allows unique value only
-        for(Country country : continent.getD_Countries()){
-            adjCountryMainSet.addAll(country.getD_AdjacentCountries());
+        HashSet<Country> l_adjCountryMainSet = new HashSet<>(); ////hash set allows unique value only
+        for(Country l_country : p_Continent.getD_Countries()){
+            l_adjCountryMainSet.addAll(l_country.getD_AdjacentCountries());
         }
 
-        System.out.println(adjCountryMainSet);
+        System.out.println(l_adjCountryMainSet);
 
-        for(Continent remainingContinent : map.getD_Continents()){
-            if(!continent.equals(remainingContinent)){
+        for(Continent l_remainingContinent : p_Map.getD_Continents()){
+            if(!p_Continent.equals(l_remainingContinent)){
                 //this will be processed if there is any relation between two continents.
                 //also it will return true ony if both the continents are different.
-                if(!Collections.disjoint(adjCountryMainSet,remainingContinent.getD_Countries())){
+                if(!Collections.disjoint(l_adjCountryMainSet,l_remainingContinent.getD_Countries())){
                     System.out.println ("Inside the disjoint method");
                     //There are some countries which are common.
-                    adjacentContinents.add(remainingContinent);
+                    l_adjacentContinents.add(l_remainingContinent);
 
                 }
             }
         }
-        return adjacentContinents;
+        return l_adjacentContinents;
     }
 
     /**
@@ -232,15 +232,15 @@ public class MapValidator {
      * @param map refers to map object.
      * @throws InvalidMapException throws InvalidMapException if map is not valid.
      */
-    public static void validateCountryBelongsToOneContinent(Map map) throws InvalidMapException{
-        HashMap<Country, Integer> countryBelongsToContinentCount = new HashMap<>();
+    public static void validateCountryBelongsToOneContinent(Map p_Map) throws InvalidMapException{
+        HashMap<Country, Integer> l_countryBelongsToContinentCount = new HashMap<>();
 
-        for(Continent continent : map.getD_Continents()){
-            for(Country country : continent.getD_Countries()){
-                if(!countryBelongsToContinentCount.containsKey(country)){
-                    countryBelongsToContinentCount.put(country,1);
+        for(Continent l_continent : p_Map.getD_Continents()){
+            for(Country l_country : l_continent.getD_Countries()){
+                if(!countryBelongsToContinentCount.containsKey(l_country)){
+                    countryBelongsToContinentCount.put(l_country,1);
                 }else{
-                    throw new InvalidMapException("Country:"+country.getD_CountryName()+"must belong to only one continent.");
+                    throw new InvalidMapException("Country:"+l_country.getD_CountryName()+"must belong to only one continent.");
                 }
             }
         }
