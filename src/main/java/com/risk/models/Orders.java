a584300d930b11a5bc40db0fc383eb16a2fb7700
PARTHNAVSARIWALA <parthnavsariwala29@gmail.com>
2021-02-18 18:04:41 +0530

    JAVA DOC for the Orders.java methods

--- src/main/java/com/risk/models/Orders.java
+++ src/main/java/com/risk/models/Orders.java
@@ -7,18 +7,34 @@ public class Orders {
     Country d_Countries;
     int d_numberOfArmies;
 
+    /**
+     * Sets country Id
+     * @param d_countryId country Id
+     */
     public void setD_countryId(int d_countryId) {
         this.d_countryId = d_countryId;
     }
 
+    /**
+     * Sets number of armies
+     * @param d_numberOfArmies number of armies
+     */
     public void setD_numberOfArmies(int d_numberOfArmies) {
         this.d_numberOfArmies = d_numberOfArmies;
     }
 
+    /**
+     * Get Id of country
+     * @return  country ID
+     */
     public int getD_countryId() {
         return d_countryId;
     }
 
+    /**
+     * Get number of armies
+     * @return number of armies
+     */
     public int getD_numberOfArmies() {
         return d_numberOfArmies;
     }
