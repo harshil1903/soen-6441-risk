7d0054ccc6ec99cdd9d6408c4b8e1e82642cad39
HARSH <harshp1898@gmail.com>
2021-02-20 12:13:05 +0530

    Created new jUnit test suite class

---
+++ src/test/java/com/risk/maputils/MapUtilsTestSuite.java
@@ -0,0 +1,15 @@
+package com.risk.maputils;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Harsh
+ */
+
+@RunWith(Suite.class)
+@SuiteClasses({MapValidatorTest.class})
+public class MapUtilsTestSuite {
+
+}
