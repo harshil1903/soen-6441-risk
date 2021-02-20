e527e15515c0d0e2be1ce91d804eb2ce90077976
HARSH <harshp1898@gmail.com>
2021-02-21 02:20:39 +0530

    Created methods validateMapForSubgraph and validateContinentForSubgraph

--- src/test/java/com/risk/maputils/MapValidatorTest.java
+++ src/test/java/com/risk/maputils/MapValidatorTest.java
@@ -41,6 +41,7 @@ public class MapValidatorTest {
 
     }
 
+
     /**
      * This method is executed before every test method.
      */
@@ -48,11 +49,12 @@ public class MapValidatorTest {
     public void beforeTest(){
         S_Continent.setD_ContinentName(d_continentName);
         S_Continent.setD_ContinentValue(d_controlValue);
-        S_Country.setD_CountryName("England");
+        S_Country.setD_CountryName("Country No.2");
 
         d_continentList = new ArrayList<>();
         d_continentList.add(S_Continent);
     }
+
 
     /**
      * This method is used for checking whether the map is null or not.
@@ -65,6 +67,7 @@ public class MapValidatorTest {
     }
 
 
+
     /** This method is used to validate whether the continent is null or not.
      * @throws InvalidMapException invalid map exception.
      */
@@ -73,4 +76,45 @@ public class MapValidatorTest {
         S_Map.setD_Continents(d_continentList);
         MapValidator.validateContinents(S_Map);
     }
+
+
+    /**
+     * This method is used to test whether the continents in the map form subgraph or not
+     * @throws InvalidMapException invalid map exception.
+     */
+    @Test (expected = InvalidMapException.class)
+    public void validateMapForSubgraph() throws InvalidMapException{
+        assertFalse(MapValidator.isMapConnectedGraph(S_Map));
+    }
+
+
+    /**
+     * This method is used to test whether continent from any graph forms subgraph or not by its countries.
+     * @throws InvalidMapException invalid map exception.
+     */
+    @Test
+    public void validateContinentForSubgraph() throws InvalidMapException{
+        List<Country> l_CountryList = new ArrayList<>();
+        l_CountryList.add(S_Country);
+        Country l_NewCountry = new Country();
+        l_NewCountry.setD_CountryName("Country No.2");
+        l_CountryList.add(S_Country);
+        S_Continent.setD_Countries(l_CountryList);
+        assertEquals(MapValidator.isContinentConnectedGraph(S_Continent,S_Map),true);
+
+        List<Country> l_AdjCountryList =new ArrayList<>();
+        l_AdjCountryList.add(S_Country);
+        l_NewCountry.setD_AdjacentCountries(l_AdjCountryList);
+
+        l_AdjCountryList = new ArrayList<>();
+        l_AdjCountryList.add(l_NewCountry);
+        S_Country.setD_AdjacentCountries(l_AdjCountryList);
+
+        l_CountryList.remove(1);
+        l_CountryList.add(l_NewCountry);
+        S_Continent.setD_Countries(l_CountryList);
+        assertTrue(MapValidator.isContinentConnectedGraph(S_Continent,S_Map));
+    }
+
+
 }
