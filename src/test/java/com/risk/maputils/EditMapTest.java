4924486f6278d1ff9cc02fba9a71610b08de6938
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-02-22 19:06:25 +0530

    Added editMapFileNotPresent

--- src/test/java/com/risk/maputils/EditMapTest.java
+++ src/test/java/com/risk/maputils/EditMapTest.java
@@ -16,5 +16,15 @@ public class EditMapTest {
     public void editMapFilePresent() throws InvalidMapException {
         assertFalse(EditMap.editMap("europe").d_Continents.isEmpty());
     }
-    
+
+    /**
+     * Tests if map file doesn't exist
+     *
+     * @throws InvalidMapException if Map is invalid.
+     */
+    @Test
+    public void editMapFileNotPresent() throws InvalidMapException {
+        assertTrue(EditMap.editMap("file not present").d_Continents.isEmpty());
+    }
+
 }
