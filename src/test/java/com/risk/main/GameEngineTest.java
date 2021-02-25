6018dd5d404ac41b8600ef8d39de4c4e7a140abc
Harshil Patel <harshilpatel1903@gmail.com>
2021-02-25 16:48:31 +0530

    Test modifications

--- src/test/java/com/risk/main/GameEngineTest.java
+++ src/test/java/com/risk/main/GameEngineTest.java
@@ -1,10 +1,18 @@
 package com.risk.main;
 
+import com.risk.controller.MapCommands;
 import com.risk.exception.InvalidMapException;
 import com.risk.gameutils.AssignCountries;
 import com.risk.maputils.EditMap;
+import com.risk.maputils.ShowMap;
+import com.risk.models.Continent;
+import com.risk.models.Country;
+import com.risk.models.Map;
 import com.risk.models.Player;
 import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 import static com.risk.main.Main.d_Map;
@@ -16,7 +24,7 @@ import static com.risk.main.Main.d_PlayerList;
  * @author Harshil
  */
 public class GameEngineTest {
-    Player d_player1, d_player2;
+    Player d_player1, d_player2, d_player3, d_player4;
 
     /**
      * This method is executed before every test method.
@@ -25,42 +33,87 @@ public class GameEngineTest {
      */
     @Before
     public void beforeTest() throws InvalidMapException {
+
         try {
             d_Map = new EditMap().editMap("europe");
         } catch (Exception e) {
             throw new InvalidMapException(e.getMessage());
         }
+
         d_player1 = new Player("TestPlayer1");
         d_PlayerList.add(d_player1);
         d_player2 = new Player("TestPlayer2");
         d_PlayerList.add(d_player2);
+        d_player3 = new Player("TestPlayer3");
+        d_PlayerList.add(d_player3);
+        d_player4 = new Player("TestPlayer4");
+        d_PlayerList.add(d_player4);
+
         AssignCountries.assignCountries();
+
+
+    }
+
+    /**
+     * This method is executed after every test method.
+     *
+     * @throws InvalidMapException the invalid map exception
+     */
+    @After
+    public void afterTest() throws InvalidMapException
+    {
+        System.out.println();
+        d_Map.getD_Continents().clear();
+        d_PlayerList.clear();
     }
 
     /**
      * Test reinforcement armies count that is assigned to the player.
      */
     @Test
-    public void testReinforcementArmies() {
+    public void testReinforcementArmies() throws InvalidMapException{
+
+        System.out.println("\n TEST : Counting reinforcement armies assigned to a player");
+
         GameEngine.assignReinforcementArmies();
 
-        assertEquals(4, d_player1.getD_Armies());
+        System.out.println("Player 1 country count : " + d_player1.getD_AssignedCountries().size());
+        System.out.println("Player 2 country count : " + d_player2.getD_AssignedCountries().size());
+
+        System.out.println("Player 1 reinforcement army count : " + d_player1.getD_Armies());
+        System.out.println("Player 2 reinforcement army count : " + d_player2.getD_Armies());
+
+        assertEquals(3, d_player1.getD_Armies());
+        assertEquals(3, d_player2.getD_Armies());
+
     }
 
     /**
      * player cannot deploy more armies that there is in their reinforcement pool.
      */
     @Test
-    public void testDeployMoreThanReinforcementArmies() {
+    public void testDeployMoreThanReinforcementArmies() throws InvalidMapException{
+
+        System.out.println("\n TEST : Testing that player cannot deploy more armies than he has in his reinforcement pool");
+
         GameEngine.assignReinforcementArmies();
 
-        String l_countryName = d_player1.getD_AssignedCountries().get(0).getD_CountryName();
+        System.out.println("Player 1 country count : " + d_player1.getD_AssignedCountries().size());
+        System.out.println("Player 2 country count : " + d_player2.getD_AssignedCountries().size());
+
+        System.out.println("Player 1 reinforcement army count : " + d_player1.getD_Armies());
+        System.out.println("Player 2 reinforcement army count : " + d_player2.getD_Armies());
+
+        String l_countryName1 = d_player1.getD_AssignedCountries().get(0).getD_CountryName();
+        String l_countryName2 = d_player2.getD_AssignedCountries().get(0).getD_CountryName();
         int l_numberOfArmies = 6;
 
-        assertFalse(d_player1.deployOrder(l_countryName, l_numberOfArmies));
+        System.out.println("Player 1 trying to deploy 6 armies");
+        assertFalse(d_player1.deployOrder(l_countryName1, l_numberOfArmies));
 
         l_numberOfArmies = 2;
-        assertTrue(d_player1.deployOrder(l_countryName, l_numberOfArmies));
+        System.out.println("Player 2 trying to deploy 2 armies");
+        assertTrue(d_player2.deployOrder(l_countryName2, l_numberOfArmies));
     }
 
 }
