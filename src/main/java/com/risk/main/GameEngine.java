3d1323358eb6cae87b1dc038d1ae48f78fb2d084
Harshil Patel <harshilpatel1903@gmail.com>
2021-02-25 15:01:51 +0530

    Continent Control Value addition to reinforcement armies

--- src/main/java/com/risk/main/GameEngine.java
+++ src/main/java/com/risk/main/GameEngine.java
@@ -2,6 +2,7 @@ package com.risk.main;
 
 import com.risk.controller.GameCommands;
 import com.risk.gameutils.AssignCountries;
+import com.risk.models.Continent;
 import com.risk.models.Country;
 import com.risk.models.Orders;
 import com.risk.models.Player;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import static com.risk.main.Main.d_Map;
 import static com.risk.main.Main.d_PlayerList;
 
 /**
@@ -109,9 +111,26 @@ public class GameEngine {
     public static void assignReinforcementArmies() {
         for (Player l_player : d_PlayerList) {
             int l_countriesOwned = l_player.getD_AssignedCountries().size();
-            int l_reinforcementCount;
+            int l_reinforcementCount, l_flag;
+            String l_playerName = l_player.getD_PlayerName();
 
             l_reinforcementCount = Math.max((l_countriesOwned / 3), 3);
+
+            for(Continent l_continent : d_Map.getD_Continents()){
+                l_flag = 0;
+                for(Country l_country : l_continent.getD_Countries()){
+                    if(!l_country.getD_Player().getD_PlayerName().equals(l_playerName))
+                    {
+                        l_flag = 1;
+                        break;
+                    }
+                }
+                if(l_flag == 0)
+                {
+                    l_reinforcementCount += l_continent.getD_ContinentValue();
+                }
+            }
+
             l_player.setD_Armies(l_reinforcementCount);
         }
     }
