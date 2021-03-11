2c2e91ee7007c6067736049567bc648e4e0baf3c
Harshil Patel <harshilpatel1903@gmail.com>
2021-03-11 13:04:05 +0530

    Added possible list of functions in Phase.java

--- src/main/java/com/risk/phases/Phase.java
+++ src/main/java/com/risk/phases/Phase.java
@@ -1,4 +1,60 @@
 package com.risk.phases;
 
-public class Phase {
+import com.risk.main.GameEngine;
+import com.risk.main.Main;
+import com.risk.main.MapEngine;
+
+public abstract class Phase {
+
+    GameEngine d_gameEngine;
+    MapEngine d_mapEngine;
+    Main d_main;
+
+    Phase(Main p_main){
+        d_main = p_main;
+    }
+
+    Phase(GameEngine p_gameEngine){
+        d_gameEngine = p_gameEngine;
+    }
+
+    Phase(MapEngine p_mapEngine){
+        d_mapEngine = p_mapEngine;
+    }
+
+    /*
+    Mapedit
+        PreLoad
+            editmap()
+        PostLoad
+            showmap()
+            editmap()
+            validatmap()
+            editcountry()
+            editcontinent()
+            editneighbor()
+            showmap()
+
+     Game
+        PreLoad
+            loadmap()
+        Postload
+            showmap()
+            gameplayer()
+            assignocuntries()
+        Main Game
+            showmap()
+            reinforce()
+            issueorder()
+            executeorder()
+     */
+
+
+
+    /**
+     *  Common method to all States.
+     */
+    public void printInvalidCommandMessage() {
+        System.out.println("Invalid command in state " + this.getClass().getSimpleName() );
+    }
 }
