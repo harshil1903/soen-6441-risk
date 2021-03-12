d37dc40fa60f34e0580214d1f304990e7fa444db
PARTHNAVSARIWALA <parthnavsariwala29@gmail.com>
2021-03-12 17:35:30 +0530

    Basic structure of Blockade class

--- src/main/java/com/risk/orders/Blockade.java
+++ src/main/java/com/risk/orders/Blockade.java
@@ -1,20 +1,47 @@
 package com.risk.orders;
 
-public class Blockade implements Order{
+import com.risk.models.Country;
+import com.risk.models.Player;
+
+/**
+ * The type Blockade
+ *
+ * @author Parth
+ * @author Harsh
+ */
+
+public class Blockade implements Order {
+
+    String d_countryName;
+    Country d_country;
+    Player d_player;
+
+    /**
+     * constructor of Blockade class
+     *
+     * @param p_player      Player Id
+     * @param p_countryName Country Id
+     */
+
+    public Blockade(Player p_player, String p_countryName) {
+        this.d_countryName = p_countryName;
+        this.d_player = p_player;
+        d_country = new Country();
+    }
 
     /**
      * Valid boolean.
      *
      * @return the boolean
      */
-    public boolean valid(){
+    public boolean valid() {
         return false;
-    };
+    }
 
     /**
      * Execute.
      */
-    public void execute(){
+    public void execute() {
 
-    };
+    }
 }
