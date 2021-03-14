563d948ca92befa9e01e5e809a93e8dfcc8bf3d6
HARSH <harshp1898@gmail.com>
2021-03-14 11:55:43 +0530

    Created Bomb test class and added basic javadoc

---
+++ src/test/java/com/risk/orders/BombTest.java
@@ -0,0 +1,17 @@
+package com.risk.orders;
+
+import com.risk.models.Country;
+import com.risk.models.Player;
+
+import java.util.ArrayList;
+
+/**
+ * To Test the Bomb functionality in the game i.e  on using the bomb card, the target country loses
+ * half of their army units.
+ *
+ * @author Harsh
+ * @author Parth
+ */
+public class BombTest {
+
+}
