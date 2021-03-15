bfe502d026546c40557174546d754962a4940aa9
HARSH <harshp1898@gmail.com>
2021-03-15 17:25:54 +0530

    Added test case for verifying successful airlift operation in the Airlift Test file

--- src/test/java/com/risk/orders/AirliftTest.java
+++ src/test/java/com/risk/orders/AirliftTest.java
@@ -13,8 +13,8 @@ import java.util.ArrayList;
 
 import static com.risk.main.Main.d_Map;
 import static com.risk.main.Main.d_PlayerList;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
+
 /**
  * To Test the Airlift card functionality in the game i.e advance some armies from one of the current player’s
  * territories to any another territory that is owned by the player.
@@ -107,7 +107,28 @@ public class AirliftTest {
 
     }
 
-    
+    @Test
+    public void testSuccessfulAirliftOperation(){
+        String l_sourceCountryName = d_player1.getD_AssignedCountries().get(3).getD_CountryName();
+        String l_targetCountryName = d_player1.getD_AssignedCountries().get(2).getD_CountryName();
+
+        d_sourceCountry = new Country();
+        d_sourceCountry = d_sourceCountry.getCountryFromCountryName(l_sourceCountryName);
+        d_sourceCountry.setD_NumberOfArmies(10);
+
+        d_targetCountry = new Country();
+        d_targetCountry = d_targetCountry.getCountryFromCountryName(l_targetCountryName);
+        d_targetCountry.setD_NumberOfArmies(4);
+
+        Airlift airlift = new Airlift(d_player1,l_sourceCountryName,l_targetCountryName,5);
+        airlift.execute();
+
+        assertEquals(d_sourceCountry.getD_NumberOfArmies(),5);
+        assertEquals(d_targetCountry.getD_NumberOfArmies(),9);
+
+        System.out.println("After Performing Airlift, the armies in the source country "+l_sourceCountryName+" are: "+d_sourceCountry.getD_NumberOfArmies());
+        System.out.println("After Performing Airlift, the armies in the target country "+l_targetCountryName+" are: "+d_targetCountry.getD_NumberOfArmies());
+    }
 
 
 
