0a9117f17a6c6abc2220af84cdcdca0340f1b705
Chirag29 <chiragpatel2965@gmail.com>
2021-03-15 20:04:22 +0530

    Added notify method

--- src/main/java/com/risk/observable/LogEntryBuffer.java
+++ src/main/java/com/risk/observable/LogEntryBuffer.java
@@ -1,10 +1,37 @@
 package com.risk.observable;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Observable class
  *
  * @author Chirag
  */
 public class LogEntryBuffer {
+    FileWriter d_logWriter;
 
+    LogEntryBuffer() {
+        String l_path = "src/main/resources/";
+        String l_fileName = "LogEntry.log";
+        FileWriter l_logWriter;
+        try {
+            this.d_logWriter = new FileWriter(l_path + l_fileName, false);
+            System.out.println("New File Created");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void notify(String p_data) {
+        try {
+            System.out.println("Data will be appended");
+            String l_data = "Log: " + p_data + "\n";
+            System.out.println(l_data);
+            d_logWriter.write(l_data);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
