36f6e1af345c63433bf8a19fe77ca806e47a91c4
HARSH <harshp1898@gmail.com>
2021-03-16 22:59:51 +0530

    Added condition for checking bomb card in bomb file for successful operation

--- src/main/java/com/risk/orders/Bomb.java
+++ src/main/java/com/risk/orders/Bomb.java
@@ -39,32 +39,38 @@ public class Bomb implements Order {
 
         //here we have to check whether both country is adjecent or not?
         //here firstly check if player have a bomb card or not after chirag make cardlist of each player
-
-        if (d_country.getD_NumberOfArmies() > 0) {
-            ArrayList<String> l_countriesOwnedList = new ArrayList<>();
-            ArrayList<String> l_adjacentList = new ArrayList<>();
-            for (Country l_country : d_player.getD_AssignedCountries()) {
-                l_countriesOwnedList.add(l_country.getD_CountryName());
-            }
-
-            for (Country l_country : d_player.getD_AssignedCountries()) {
-                for (Country l_adjCountry : l_country.getD_AdjacentCountries()) {
-                    l_adjacentList.add(l_adjCountry.getD_CountryName());
+        if(d_player.getD_cardList().contains("bomb")){
+            if (d_country.getD_NumberOfArmies() > 0) {
+                ArrayList<String> l_countriesOwnedList = new ArrayList<>();
+                ArrayList<String> l_adjacentList = new ArrayList<>();
+                for (Country l_country : d_player.getD_AssignedCountries()) {
+                    l_countriesOwnedList.add(l_country.getD_CountryName());
                 }
-            }
-            if (!l_adjacentList.contains(d_countryName)) {
-                System.out.println(d_countryName + " not adjacent with " + d_player.getD_PlayerName() + "countries");
+
+                for (Country l_country : d_player.getD_AssignedCountries()) {
+                    for (Country l_adjCountry : l_country.getD_AdjacentCountries()) {
+                        l_adjacentList.add(l_adjCountry.getD_CountryName());
+                    }
+                }
+                    if (!l_adjacentList.contains(d_countryName)) {
+                        System.out.println(d_countryName + " not adjacent with " + d_player.getD_PlayerName() + "countries");
+                        return false;
+                    }
+
+                    if (l_countriesOwnedList.contains(d_countryName)) {
+                        System.out.println(d_player.getD_PlayerName() + " can not attack bomb on your own country");
+                        return false;
+                    }
+
+                    return true;
+            } else {
+                System.out.println(d_countryName + " has a 0 army so you can not apply bomb order there");
                 return false;
             }
 
-            if (l_countriesOwnedList.contains(d_countryName)) {
-                System.out.println(d_player.getD_PlayerName() + " can not attack bomb on your own country");
-                return false;
-            }
 
-            return true;
-        } else {
-            System.out.println(d_countryName + " has a 0 army so you can not apply bomb order there");
+        }else{
+            System.out.println("Player does not contain bomb card");
             return false;
         }
     }
@@ -72,7 +78,7 @@ public class Bomb implements Order {
     /**
      * Execute.
      */
-    public void execute() {
+    public void execute(){
         if (valid()) {
             int l_previousArmy = d_country.getCountryFromCountryName(d_countryName).getD_NumberOfArmies();
             System.out.println("Before Bomb Card number of army in "+d_countryName+" is : "+d_country.getD_NumberOfArmies());
