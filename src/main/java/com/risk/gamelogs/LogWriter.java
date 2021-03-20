8d9c1e62b785254feda70e81a14ade4b011cbfaf
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-03-20 20:01:18 +0530

    updated LogWriter

--- src/main/java/com/risk/gamelogs/LogWriter.java
+++ src/main/java/com/risk/gamelogs/LogWriter.java
@@ -1,9 +1,31 @@
 package com.risk.gamelogs;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class LogWriter implements Observer {
+    String l_fileName = "src/main/resources/LogEntry.log";
+    FileWriter d_logWriter;
+
+    {
+        try {
+            d_logWriter = new FileWriter(l_fileName, false);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
 
     @Override
     public void update(Observable p_observable_state) {
+        try {
+            String l_fileName = "src/main/resources/LogEntry.log";
+            d_logWriter.write("What to be added");
+            d_logWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
