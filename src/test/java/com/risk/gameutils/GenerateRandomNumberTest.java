c65305b8cead91c970b80f6e69ad5c4cc4e10d08
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-03-22 15:55:50 +0530

    Updated GenerateRandomNumberTest

--- src/test/java/com/risk/gameutils/GenerateRandomNumberTest.java
+++ src/test/java/com/risk/gameutils/GenerateRandomNumberTest.java
@@ -7,20 +7,31 @@ import static com.risk.gameutils.GenerateRandomNumber.getRandomNumber;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Test class of GenerateRandomNumber in gameutils
+ *
+ * @author Chirag
+ */
 public class GenerateRandomNumberTest {
+    /**
+     * Checks if number is in Range with Upper Bound while Lower Bound is zero.
+     */
     @Test
-    public void checkNumberInRange(){
-        int getNum=getRandomNumber(1000);
-        System.out.println("The Random Number Generated is:"+getNum);
-        assertTrue(getNum>=0 && getNum<1000?true:false);
-        assertFalse(getNum>=1000?true:false);
-}
+    public void checkNumberInRange() {
+        int l_getNum = getRandomNumber(1000);
+        System.out.println("The Random Number Generated is:" + l_getNum);
+        assertTrue(l_getNum >= 0 && l_getNum < 1000 ? true : false);
+        assertFalse(l_getNum >= 1000 ? true : false);
+    }
 
+    /**
+     * Checks if number is in Range with Upper Bound and Lower Bound.
+     */
     @Test
-    public void checkNumberInLowerAndUpperBound(){
-        int getNum=getRandomNumber(1000,5);
-        System.out.println("The Random Number Generated is:"+getNum);
-        assertTrue(getNum>=5 && getNum<1000?true:false);
-        assertFalse(getNum>=1000?true:false);
+    public void checkNumberInLowerAndUpperBound() {
+        int l_getNum = getRandomNumber(1000, 5);
+        System.out.println("The Random Number Generated is:" + l_getNum);
+        assertTrue(l_getNum >= 5 && l_getNum < 1000 ? true : false);
+        assertFalse(l_getNum >= 1000 ? true : false);
     }
 }
