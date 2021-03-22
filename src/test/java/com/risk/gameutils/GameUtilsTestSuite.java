d21a8806a1c0a567bc6e2bd6e669a45d76e9a995
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-03-22 16:10:22 +0530

    updated GameUtilsTestSuite

--- src/test/java/com/risk/gameutils/GameUtilsTestSuite.java
+++ src/test/java/com/risk/gameutils/GameUtilsTestSuite.java
@@ -1,4 +1,14 @@
 package com.risk.gameutils;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test suite for gameutils.
+ *
+ * @author Chirag
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({GenerateRandomNumberTest.class})
 public class GameUtilsTestSuite {
 }
