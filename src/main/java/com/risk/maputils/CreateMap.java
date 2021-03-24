b04144e4f0a3e12f2cedfe3b28c655983b422636
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-03-24 12:40:21 +0530

    added CreateMap LoadMap

---
+++ src/main/java/com/risk/maputils/CreateMap.java
@@ -0,0 +1,7 @@
+package com.risk.maputils;
+
+/**
+ * Creates a new map from scratch if not present
+ */
+public class CreateMap {
+}
---
+++ src/main/java/com/risk/maputils/LoadMap.java
@@ -0,0 +1,8 @@
+package com.risk.maputils;
+
+/**
+ * Loads the data from the map file and stores it in a variable
+ * @author Chirag
+ */
+public class LoadMap {
+}
