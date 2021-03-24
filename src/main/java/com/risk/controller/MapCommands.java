f0cfa626124d142258100587e17bba2eab84f5ad
Rishabh <rishabhpatel12121@gmail.com>
2021-03-24 18:09:34 +0530

    Added log notify for exception of wrong number of arguments provided

--- src/main/java/com/risk/controller/MapCommands.java
+++ src/main/java/com/risk/controller/MapCommands.java
@@ -211,7 +211,7 @@ public class MapCommands {
 
                 } catch (Exception e) {
                     System.out.println("Wrong number of Arguments provided. remove option has 1 argument");
-                    //d_Log.notify("Wrong number of Arguments provided. remove option has 1 argument");
+                    d_Log.notify("Wrong number of Arguments provided. remove option has 1 argument");
                     return;
                 }
             } else {
