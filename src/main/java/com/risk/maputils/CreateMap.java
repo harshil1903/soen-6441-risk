b8d0e86a87f5205a7688cfb64b8140c81481a5a4
Rishabh <rishabhpatel12121@gmail.com>
2021-03-24 17:50:05 +0530

    Map file missing notification added

--- src/main/java/com/risk/maputils/CreateMap.java
+++ src/main/java/com/risk/maputils/CreateMap.java
@@ -1,6 +1,9 @@
 package com.risk.maputils;
 
 import com.risk.models.Map;
+
+import static com.risk.main.Main.d_Log;
+
 /**
  * Creates a new map from scratch if not present
  */
@@ -11,6 +14,7 @@ public class CreateMap {
     public static Map createMap(Map p_Map) {
         p_Map = new Map();
         System.out.println("Map file not presented will be created from scratch");
+        d_Log.notify("Map file not presented will be created from scratch");
         p_Map.d_isEmpty = true;
         return p_Map;
     }
