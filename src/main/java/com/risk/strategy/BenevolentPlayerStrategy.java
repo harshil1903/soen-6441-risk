49cb889e9ff9a3a7944dbaca052c0bebbac1255a
PARTHNAVSARIWALA <parthnavsariwala29@gmail.com>
2021-04-03 13:34:48 +0530

    update javadoc for BenevolentPlayerStrategy.java

--- src/main/java/com/risk/strategy/BenevolentPlayerStrategy.java
+++ src/main/java/com/risk/strategy/BenevolentPlayerStrategy.java
@@ -13,17 +13,30 @@ public class BenevolentPlayerStrategy extends PlayerStrategy {
     /**
      * Instantiates a new Player strategy.
      *
-     * @param p_player  the p player
-     * @param p_country the p map
+     * @param p_player  Player object
+     * @param p_country List of country
      */
     BenevolentPlayerStrategy(Player p_player, List<Country> p_country) {
         super(p_player, p_country);
     }
 
+    /**
+     * Decide the country to attack to
+     * Benevolent Player doesn't attack so this method return null
+     *
+     * @return null
+     */
+
     protected Country toAttack() {
         return null;
     }
 
+    /**
+     * Decide which country should be defend
+     * Benevolent Player decide to defend its country with the most armies
+     *
+     * @return current player country with most number of armies
+     */
     protected Country toDefend() {
         Country l_myMaxArmies = d_player.getD_AssignedCountries().get(0);
         for (Country l_tempCountry : d_country) {
@@ -34,14 +47,29 @@ public class BenevolentPlayerStrategy extends PlayerStrategy {
         return l_myMaxArmies;
     }
 
+    /**
+     * Decide where to launch an attack from. The Defensive player does not attack, so it returns null
+     *
+     * @return null
+     */
     protected Country toAttackFrom() {
         return null;
     }
 
+    /**
+     * Decide where armies are moved to. The Defensive player does not move, so it returns null
+     *
+     * @return null
+     */
     protected Country toMoveFrom() {
         return null;
     }
 
+    /**
+     * Create an order benevolent player can use only deploy order
+     *
+     * @return created order
+     */
     @Override
     public Order createOrder() {
         Random l_rand = new Random();
