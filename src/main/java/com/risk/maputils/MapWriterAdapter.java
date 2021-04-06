e780e2ed9824676e6e51bd0989456ef9a8426654
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-04-06 17:28:51 +0530

    added javadoc MapWriterAdapter

--- src/main/java/com/risk/maputils/MapWriterAdapter.java
+++ src/main/java/com/risk/maputils/MapWriterAdapter.java
@@ -1,5 +1,9 @@
 package com.risk.maputils;
 
 
+/**
+ * Adapter class for MapWriter which will help in writing data back to map file.
+ * @author Chirag
+ */
 public class MapWriterAdapter extends MapWriter{
 }
