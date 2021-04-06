2beb032b009cd4f880f06e9a2b9f87d08ae3242b
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-04-06 17:06:55 +0530

    added Parameterized constructor for LoadMapAdapter

--- src/main/java/com/risk/maputils/LoadMapAdapter.java
+++ src/main/java/com/risk/maputils/LoadMapAdapter.java
@@ -1,4 +1,26 @@
 package com.risk.maputils;
 
-public class LoadMapAdapter {
+
+import com.risk.models.Map;
+
+/**
+ * Adapter class for LoadMap that helps in accepting conquest file format map.
+ *
+ * @author Chirag
+ */
+public class LoadMapAdapter extends LoadMap {
+    public static Map d_map = new Map();
+
+    private LoadConquestMap d_loadConquestMap;
+
+    /**
+     * Parameterized constructor for LoadMapAdapter.
+     *
+     * @param p_loadConquestMap object of LoadConquestMap
+     */
+    public LoadMapAdapter(LoadConquestMap p_loadConquestMap) {
+        this.d_loadConquestMap = p_loadConquestMap;
+    }
+
+
 }
