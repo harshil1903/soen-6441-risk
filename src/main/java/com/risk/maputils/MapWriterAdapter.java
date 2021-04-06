0b5ba82d3c4191131dc555372b634b0c28d711d8
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-04-06 17:31:49 +0530

    added constructor for MapWriterAdapter

--- src/main/java/com/risk/maputils/MapWriterAdapter.java
+++ src/main/java/com/risk/maputils/MapWriterAdapter.java
@@ -1,9 +1,24 @@
 package com.risk.maputils;
 
 
+import com.risk.models.Continent;
+
 /**
  * Adapter class for MapWriter which will help in writing data back to map file.
+ *
  * @author Chirag
  */
-public class MapWriterAdapter extends MapWriter{
+public class MapWriterAdapter extends MapWriter {
+
+    private static ConquestMapWriter d_conquestMapWriter;
+
+    /**
+     * Parameterized constructor for MapWriterAdapter.
+     * @param p_conquestMapWriter object of ConquestMapWriter
+     */
+    public MapWriterAdapter(ConquestMapWriter p_conquestMapWriter) {
+        this.d_conquestMapWriter = p_conquestMapWriter;
+    }
+
+
 }
