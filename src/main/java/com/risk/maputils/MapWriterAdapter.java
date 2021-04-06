e0abb67041f555e6929f8bfa9866c330e7d04f2d
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-04-06 17:34:46 +0530

    added writeMapFile for MapWriterAdapter

--- src/main/java/com/risk/maputils/MapWriterAdapter.java
+++ src/main/java/com/risk/maputils/MapWriterAdapter.java
@@ -1,7 +1,8 @@
 package com.risk.maputils;
 
+import com.risk.models.Map;
 
-import com.risk.models.Continent;
+import java.io.File;
 
 /**
  * Adapter class for MapWriter which will help in writing data back to map file.
@@ -14,11 +15,23 @@ public class MapWriterAdapter extends MapWriter {
 
     /**
      * Parameterized constructor for MapWriterAdapter.
+     *
      * @param p_conquestMapWriter object of ConquestMapWriter
      */
     public MapWriterAdapter(ConquestMapWriter p_conquestMapWriter) {
         this.d_conquestMapWriter = p_conquestMapWriter;
     }
 
+
+    /**
+     * This method overrides writes the writeMapFile method of MapWriter class.
+     *
+     * @param p_map  object of the map which is being processed.
+     * @param p_file object of file to store map data.
+     */
+
+    public static void writeMapFile(Map p_map, File p_file) {
+        d_conquestMapWriter.writeConquestMapFile(p_map, p_file);
+    }
 
 }
