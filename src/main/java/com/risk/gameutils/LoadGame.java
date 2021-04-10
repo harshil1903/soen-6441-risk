d87f983d0f8344557c20a411a63482d9c54bf540
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-04-10 21:36:38 +0530

    added loadGame method

--- src/main/java/com/risk/gameutils/LoadGame.java
+++ src/main/java/com/risk/gameutils/LoadGame.java
@@ -1,4 +1,22 @@
 package com.risk.gameutils;
 
+import java.io.File;
+
+/**
+ * Helps in loading previously load game form the game file.
+ * @author Chirag
+ */
 public class LoadGame {
+
+
+    public static void loadGame(String p_gameFile) {
+        String l_path = "src/main/resources/";
+        String l_fileName = p_gameFile + ".game";
+        File l_game = new File(l_path + l_fileName);
+        if (l_game.exists())
+            System.out.println("Game will be loaded");
+        else
+            System.out.println("Game file not found");
+    }
 }
+
