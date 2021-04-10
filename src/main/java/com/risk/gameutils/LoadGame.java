885ddc3853cdebc6c2c53f43e531cce95c6b70e3
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-04-10 22:00:25 +0530

    added fillMap method.

--- src/main/java/com/risk/gameutils/LoadGame.java
+++ src/main/java/com/risk/gameutils/LoadGame.java
@@ -1,11 +1,18 @@
 package com.risk.gameutils;
 
+import com.risk.exception.InvalidMapException;
+import com.risk.maputils.EditMap;
+import com.risk.maputils.ShowMap;
+import com.risk.models.Continent;
+import com.risk.models.Country;
 import com.risk.models.Map;
 import com.risk.models.Player;
 import com.risk.phases.Phase;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Helps in loading previously load game form the game file.
@@ -36,21 +43,54 @@ public class LoadGame {
     public static Phase getD_GamePhase(){return d_GamePhase;}
 
 
+
+    public static void fillMap(Scanner p_gameReader) throws InvalidMapException {
+        while (p_gameReader.hasNextLine()) {
+            String l_line = p_gameReader.nextLine();
+            if (l_line.equals("")) break;
+            String[] l_parts = l_line.split(" ");
+            String p_mapName=l_parts[0];
+            d_Map= EditMap.editMap(p_mapName);
+        }
+    }
+
+
     public static void loadGame(String p_gameFile) {
         String l_path = "src/main/resources/";
         String l_fileName = p_gameFile + ".game";
         File l_game = new File(l_path + l_fileName);
-        if (l_game.exists()){
-            System.out.println("Game will be loaded");
+        if (l_game.exists()) {
+            Scanner l_gameReader = null;
+            try {
+                l_gameReader = new Scanner(l_game);
+                while (l_gameReader.hasNextLine()) {
+                    String l_line = l_gameReader.nextLine();
+                    if (l_line.equals("[map]")) {
+                        fillMap(l_gameReader);
+                    }
+//                    if (l_line.equals("[countries]")) {
+//                        getCountries(l_gameReader, p_Map);
+//                    }
+//                    if (l_line.equals("[borders]")) {
+//                        getAdjacentCountries(l_gameReader, p_Map);
+//                    }
+                }
+                System.out.println("Loaded map successfully form existing domination file");
+
+
+                System.out.println("Game will be loaded");
+
+            } catch (FileNotFoundException | InvalidMapException e) {
+                System.out.println("Game file not found");
+            }
+
 
         }
-
-        else
-            System.out.println("Game file not found");
     }
-
     public static void main(String[] args) {
         LoadGame.loadGame("Game1");
+        ShowMap.displayEditorMap(d_Map);
+
     }
 }
 
