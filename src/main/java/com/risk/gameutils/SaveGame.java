fda68151ccae709f796045e2f058dd9b8a247c3b
ChiragPatel29 <cp2911.chiragpatel@gmail.com>
2021-04-10 16:54:42 +0530

    added processPlayers

--- src/main/java/com/risk/gameutils/SaveGame.java
+++ src/main/java/com/risk/gameutils/SaveGame.java
@@ -1,6 +1,9 @@
 package com.risk.gameutils;
 
+import com.risk.exception.InvalidMapException;
+import com.risk.maputils.EditMap;
 import com.risk.models.Continent;
+import com.risk.models.Country;
 import com.risk.models.Map;
 import com.risk.models.Player;
 import com.risk.phases.Phase;
@@ -10,7 +13,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static com.risk.gameutils.AssignCountriesGame.assignCountries;
 import static com.risk.main.Main.d_Log;
+import static com.risk.main.Main.d_PlayerList;
+import static com.risk.maputils.ShowMap.displayGameMap;
 
 /**
  * Helps in saving the game so it can be loaded next time
@@ -19,37 +25,9 @@ import static com.risk.main.Main.d_Log;
  */
 public class SaveGame {
 
-
-    /**
-     * entry point for the SaveGame class helps in saving the current game
-//     *
-//     * @param p_Map        Contains the Map data
-//     * @param p_gamePhase  contains the current phase of the game
-//     * @param d_PlayerList contains the list of all the players
-     */
-    //public void saveGame(Map p_Map, Phase p_gamePhase, ArrayList<Player> d_PlayerList,String d_fileName){
-    public void saveGame(String d_fileName) {
-        String p_mapName = "europe";
-
-
-        String l_path = "src/main/resources/";
-        String l_fileName = d_fileName + ".game";
-        File l_map = new File(l_path + l_fileName);
-        FileWriter l_fileWriter;
-        try {
-            String l_content = "Write this to file please please write this to file";
-            l_fileWriter = new FileWriter(l_map, false);
-            StringBuilder l_mapData = processMap();
-            l_fileWriter.write(l_mapData.toString());
-            l_fileWriter.close();
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-
     /**
      * Process the map used in the game and add it to Game file
+     *
      * @return the Map data to be added to game file.
      */
     public StringBuilder processMap() {
@@ -63,8 +41,85 @@ public class SaveGame {
     }
 
 
-//    public static void main(String[] args) {
+    public StringBuilder processPlayers(ArrayList<Player> d_PlayerList){
+        StringBuilder l_mapData = new StringBuilder();
+        l_mapData.append("\n");
+        l_mapData.append("[players]");
+        l_mapData.append("\n");
+        for(Player player:d_PlayerList){
+            l_mapData.append(player.getD_PlayerName());
+            //l_mapData.append("\n \n");
+            l_mapData.append("\nCountries Owned: ");
+            for(Country l_country:player.getD_AssignedCountries()){
+                l_mapData.append(l_country.getD_CountryName()+" ");
+            }
+            l_mapData.append("\nCards: ");
+
+            for(String card:player.getD_cardList()){
+                l_mapData.append(card+" ");
+            }
+            l_mapData.append("\nNo of Armies:");
+            l_mapData.append(player.getD_Armies());
+            l_mapData.append("\n \n");
+        }
+        l_mapData.append("\n");
+        return l_mapData;
+    }
+
+    /**
+     * entry point for the SaveGame class helps in saving the current game
+     * //     *
+     * //     * @param p_Map        Contains the Map data
+     * //     * @param p_gamePhase  contains the current phase of the game
+     * //     * @param d_PlayerList contains the list of all the players
+     */
+    //public void saveGame(Map p_Map, Phase p_gamePhase, ArrayList<Player> d_PlayerList,String d_fileName){
+    public void saveGame(String d_fileName,ArrayList<Player> d_PlayerListGame) {
+        String p_mapName = "europe";
+
+
+        String l_path = "src/main/resources/";
+        String l_fileName = d_fileName + ".game";
+        File l_map = new File(l_path + l_fileName);
+        FileWriter l_fileWriter;
+        try {
+            String l_content = "Write this to file please please write this to file";
+            l_fileWriter = new FileWriter(l_map, false);
+            StringBuilder l_mapData = processMap();
+            l_fileWriter.write(l_mapData.toString());
+            StringBuilder l_playerData = processPlayers(d_PlayerListGame);
+            l_fileWriter.write(l_playerData.toString());
+            l_fileWriter.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+
+//    public static void main(String[] args) throws InvalidMapException {
 //        SaveGame sg = new SaveGame();
-//        sg.saveGame("Game1");
+//
+//        /**
+//         * Contains list of players.
+//         */
+//        ArrayList<Player> d_PlayerListGame = new ArrayList<Player>();
+//        Player P1 = new Player("P1");
+//        Player P2 = new Player("P2");
+//        Player P3 = new Player("P3");
+//        P1.addCard("first");
+//        P2.addCard("first");
+//        P2.addCard("first");
+//        P3.addCard("first");
+//        P3.addCard("first");
+//        P3.addCard("last");
+//        d_PlayerListGame.add(P1);
+//        d_PlayerListGame.add(P2);
+//        d_PlayerListGame.add(P3);
+//
+//        Map d_Map= EditMap.editMap("europe");
+//        assignCountries(d_PlayerListGame,d_Map);
+//        sg.saveGame("Game1",d_PlayerListGame);
+//        //displayGameMap(d_Map);
+//
 //    }
 }
